73514c0f36ba044d6492dc84d2cba98ec8560e28
Shashank <mail2shashank1683@gmail.com>
2020-05-17 20:40:30 +0530

    removed unused imports

--- Breast Cancer Classification.py
+++ Breast Cancer Classification.py
@@ -1,7 +1,5 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pandas as pd
 
 # import dataset fro sklearn datasets
 from sklearn.datasets import load_breast_cancer
